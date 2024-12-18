f0a5378edd883cabd4ce258cbfb851976017deb6
rahul97701 <singh.rahulsingh.om07@gmail.com>
2024-12-18 09:45:34 -0500

    "all changes done"

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,4 +1,21 @@
+// async function startTyping() {
+//     var typed = new Typed('.typing', {
+//         strings: [
+//             'Hey I am Prateek Singh.',
+//             'I am a student.',
+//             'I am a UI/UX Designer.',
+//             'I am a Web Developer.',
+//             'I am a Grapics Designer.'
+//         ],
+//         smartBackspace: true,
+//         loop: true,
+//         typeSpeed: 60,
+//         backSpeed: 40,
+//         backDelay: 3000,
+//         startDelay: 1000,
+//     });
 
+// }
 
 AOS.init();
 
